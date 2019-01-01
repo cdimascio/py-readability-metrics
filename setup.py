774e86ae6d8cc8489dd570b5a546ce9508695e25
Carmine DiMascio <dimascio@us.ibm.com>
2018-12-31 20:09:34 -0500

    update ver and deps

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='py-readability-metrics',
-    version='0.0.1',
+    version='0.1.1',
     description='Calculate readability scores. e.g. Flesch, Flesch-Kincaid, and more',
     author='Carmine DiMAscio',
     url='https://github.com/cdimascio/py-readability-metrics',
-    packages=['py-readabilitiy-metrics'],
-    install_requires=['nltk>=3.3'],
+    packages=['readability'],
+    install_requires=['nltk'],
     package_data={'readability': [], '': ['README.md', 'LICENSE']},
     package_dir={'readabiliity': 'readabiliity'},
     include_package_data=True,
