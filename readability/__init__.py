bf96bc0bd6ec1fb8c437fa1b491e0a6904970e70
Carmine DiMascio <dimascio@us.ibm.com>
2019-01-06 13:11:22 -0500

    increment version

--- readability/__init__.py
+++ readability/__init__.py
@@ -3,7 +3,7 @@
 Calculate gunning fog readability score
 """
 __title__ = 'py-readabilitiy-metrics'
-__version__ = '2.0.1'
+__version__ = '1.3.0'
 __author__ = 'Carmine DiMascio'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Carmine DiMascio'
